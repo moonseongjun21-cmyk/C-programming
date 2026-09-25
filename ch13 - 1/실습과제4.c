ff7faa79bd610bcdc0f0337d5ba6484d665e5ede
moonseongjun21-cmyk <moonseongjun21@gmail.com>
2026-09-25 19:25:31 +0900

    Update 실습과제4.c

--- ch13 - 1/실습과제4.c
+++ ch13 - 1/실습과제4.c
@@ -1,1 +1,25 @@
+// ****************************
+//   제목 : 교재 299p 문제1 변형
+//   날짜 : 2026년 9월 10일
+//   작성자 : 2600065 문성준
+// ****************************
 
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5];
+	int* ptr = arr;
+	int i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &(ptr[i]));
+		*(ptr + i) += 2;
+		printf("2씩 증가된 결과:%d \n", *(ptr + i));
+	}
+
+	return 0;
+}
