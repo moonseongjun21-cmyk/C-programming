47d16465023c78a5d4aa77073401cfd889ef6624
moonseongjun21-cmyk <moonseongjun21@gmail.com>
2026-09-25 20:08:19 +0900

    Update 실습과제4.c

--- ch13 - 2/실습과제4.c
+++ ch13 - 2/실습과제4.c
@@ -1,7 +1,11 @@
+// *********************************
+// 제 목 : 사전에 앞서있는 문자열 출력
+// 날 짜 : 2026년 9월 17일
+// 작성자 : 2600065 문성준
+// *********************************
+
 #define _CRT_SECURE_NO_WARNINGS
-
 #pragma warning(disable:6031)
-
 #include <stdio.h>
 
 int main(void)
