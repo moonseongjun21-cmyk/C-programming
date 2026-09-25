277a15dd39dc89b9b81c4759f0ab37f8092044ca
moonseongjun21-cmyk <moonseongjun21@gmail.com>
2026-09-25 19:48:14 +0900

    Update 실습과제5.c

--- ch13 - 1/실습과제5.c
+++ ch13 - 1/실습과제5.c
@@ -16,67 +16,93 @@ int main(void)
 
     // 배열 표현
     for (i = 0; i < 5; i++)
+    {
         scanf("%lf", &a[i]);
-
+    }
     for (i = 0; i < 5; i++)
+    {
         a[i] += 2;
-
+    }
     printf("배열 표현: ");
+    
     for (i = 0; i < 5; i++)
+    {
         printf("%.1f ", a[i]);
+    }
+
     printf("\n");
 
 
     // 배열명을 이용한 주소 표현
     for (i = 0; i < 5; i++)
+    {
         scanf("%lf", a + i);
-
+    }
     for (i = 0; i < 5; i++)
+    {
         *(a + i) += 2;
+    }
 
     printf("배열명을 이용한 주소 표현: ");
     for (i = 0; i < 5; i++)
+    {
         printf("%.1f ", *(a + i));
-    printf("\n");
-
+    }
+        printf("\n");
 
     // 포인터 변수를 이용한 배열 표현
     for (i = 0; i < 5; i++)
+    {
         scanf("%lf", ptr + i);
-
+    }
     for (i = 0; i < 5; i++)
+    {
         ptr[i] += 2;
-
+    }
+    
     printf("포인터 변수를 이용한 배열 표현: ");
+    
     for (i = 0; i < 5; i++)
+    {
         printf("%.1f ", ptr[i]);
-    printf("\n");
-
+    }
+        printf("\n");
 
     // 포인터 변수를 이용한 주소 표현
     for (i = 0; i < 5; i++)
+    {    
         scanf("%lf", ptr + i);
-
+    }
     for (i = 0; i < 5; i++)
+    {
         *(ptr + i) += 2;
+    }
 
     printf("포인터 변수를 이용한 주소 표현: ");
     for (i = 0; i < 5; i++)
+    {
         printf("%.1f ", *(ptr + i));
+    }
+    
     printf("\n");
 
 
     // 포인터에 덧셈 연산을 이용한 주소 표현
     for (i = 0; i < 5; i++)
+    {
         scanf("%lf", ptr + i);
-
+    }
     for (i = 0; i < 5; i++)
+    {
         *(ptr + i) += 2;
+    }
 
     printf("포인터 덧셈 연산을 이용한 주소 표현: ");
+    
     for (i = 0; i < 5; i++)
+    {
         printf("%.1f ", *(ptr + i));
-
+    }
     return 0;
 }
 
