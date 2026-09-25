d6af500ec66033187f7eacce9f8d0672f5291f3f
moonseongjun21-cmyk <moonseongjun21@gmail.com>
2026-09-25 17:32:05 +0900

    Update 실습과제4.c

--- ch12 - 1/실습과제4.c
+++ ch12 - 1/실습과제4.c
@@ -1,1 +1,26 @@
+// ************************
+// 제 목 : 변수들의 주소 출력
+// 날 짜 : 2026년 9월 3일
+// 작성자 : 2600065 문성
+// ************************
+
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include <stdio.h>
+
+int main(void)
+{
+	char a = 'A';
+	int b = 36;
+	double c = 3.141592;
+
+	printf("char형 변수a의주소 : %p \n", &a);
+	printf("int형 변수b의주소 : %p \n", &b);
+	printf("double형 변수c의주소 : %p", &c);
+
+	return 0;
+}
+
+
+
 
