8e78e0610ec71e5d0d9c10e33d846a80741c50a6
moonseongjun21-cmyk <moonseongjun21@gmail.com>
2026-09-25 17:42:18 +0900

    Update 실습과제2.c

--- ch12 - 2/실습과제2.c
+++ ch12 - 2/실습과제2.c
@@ -1,1 +1,24 @@
+// **********************************
+// 제 목 : 포인터를 이용한 변수 값 출력
+// 날 짜 : 2026년 9월 8일
+// 작성자 : 2600065 문성준
+// **********************************
 
+#include <stdio.h>
+
+int main(void)
+{
+    int a = -100;
+    char b = 'A';
+    double c = 3.14;
+
+    int* pa = &a;
+    char* pb = &b;
+    double* pc = &c;
+
+    printf("int형 변수 a의 값은 : %d\n", *pa);
+    printf("char형 변수 b의 값은 : %c\n", *pb);
+    printf("double형 변수 c의 값은 : %lf\n", *pc);
+
+    return 0;
+}
