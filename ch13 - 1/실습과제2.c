e078df764c9d5a1ccf874741323e83f7a01de216
moonseongjun21-cmyk <moonseongjun21@gmail.com>
2026-09-25 19:12:02 +0900

    Update 실습과제2.c

--- ch13 - 1/실습과제2.c
+++ ch13 - 1/실습과제2.c
@@ -1,1 +1,33 @@
+// ******************************
+// 제 목 : 배열 표현 -> 포인터 표현
+// 날 짜 : 2026년 9월 10일
+// 작성자 : 2600065 문성준
+// ******************************
+
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
+
+int main(void)
+{
+    int grade[5];
+    int sum = 0, i, average;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("성적을 입력하시오: ");
+        scanf("%d", (grade + i));
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+      sum += *(grade + i);  
+    }
+  
+    average = sum / 5.0;
+  
+    printf("성적 평균= %d\n", average);
+
+    return 0;
+}
 
